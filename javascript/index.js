efae0bcef5fe8420af327dac9ab6d66a971b8955
akhiravictoriasalahid-web <akhiravictoriasalahid@gmail.com>
2026-03-24 15:39:01 +0100

    Add console log, updated js

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,19 @@
 function isvalidPassword(password, username) {
     if (password.length < 8) {
+        console.log("Your Password is not long enough");
         return false;
     }
     if (password.includes(" ")) {
+        console.log("Your Password may not include spaces");
         return false;
     }
     if (password.includes(username)) {
+        console.log("Your Password may not include Username");
         return false;
+    } else {
+        console.log("Your Password is approve");
+        return true;
     }
-    return true;
 }
 
 console.log(isvalidPassword("abc123459", "user"));
